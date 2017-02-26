1ed014da023c98057fd6a96dfbcefc46fda74732
Jun <cygoh@umich.edu>
2017-02-26 13:54:34 -0800

    Added more test to inputValidation and Variable ToString method

--- app/src/test/java/com/jun/canonicalizer/CanonicalizerUnitTest.java
+++ app/src/test/java/com/jun/canonicalizer/CanonicalizerUnitTest.java
@@ -8,10 +8,13 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class CanonicalizerUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        assertEquals("\\.","\\.");
+        assertEquals('.','.');
+        assertEquals(Integer.toString(-1),"-1");
     }
 
     @Test
@@ -24,7 +27,12 @@ public class ExampleUnitTest {
         assertFalse(Canonicalizer.isValidInput("1^2x * 3.5xy = y^2 - xy + y"));
         // no multiplication between terms enclosed in brackets
         assertFalse(Canonicalizer.isValidInput("(1^2x - 3.5xy)(x) = y^2 - xy + y"));
+        assertFalse(Canonicalizer.isValidInput("(1^2x - 3.xy)(x) = y^2 - xy + y"));
+        assertFalse(Canonicalizer.isValidInput("x - ((x^2.3y^2 - x) + x) = 0"));
+        // doesn't support negative power
+        assertTrue(Canonicalizer.isValidInput("x - ((x^-2y^2 - x) + x) = 0"));
 
+        assertTrue(Canonicalizer.isValidInput("x - ((x^2y^2 - x) + x) = 0"));
         assertTrue(Canonicalizer.isValidInput("x - ((y^2 - x) + x) = 0"));
         assertTrue(Canonicalizer.isValidInput("x - (y^2 - x) = 0"));
         assertTrue(Canonicalizer.isValidInput("x^2 + 3.5xy + y = y^2 - xy + y"));
@@ -32,5 +40,4 @@ public class ExampleUnitTest {
         assertTrue(Canonicalizer.isValidInput("x = 1"));
         assertTrue(Canonicalizer.isValidInput("x - (0 - (0 - x)) = 0"));
     }
-
 }
---
+++ app/src/test/java/com/jun/canonicalizer/VariableUnitTest.java
@@ -0,0 +1,22 @@
+package com.jun.canonicalizer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jun on 2017-02-26.
+ * Unit tests for Variable Class
+ */
+public class VariableUnitTest {
+    @Test
+    public void testVariableToString() {
+        Variable var1 = new Variable("x", 3);
+        Variable var2 = new Variable("x", 0);
+        Variable var3 = new Variable("x", -1);
+
+        assertEquals(var1.toString(), "x^3");
+        assertEquals(var2.toString(), "x");
+        assertEquals(var2.toString(), "x^-1");
+    }
+}
